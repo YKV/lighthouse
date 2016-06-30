8f2296ca0fb04bba7bb4bb9133ff0f6e5046d873
Yevhen <fortranibm@gmail.com>
2016-06-30 18:47:08 -0400

    complete version

---
+++ FoodTruckP1/FoodTruckP1/GrreterDelegator.h
@@ -0,0 +1,13 @@
+//
+//  GrreterDelegator.h
+//  FoodTruckP1
+//
+//  Created by Yevhen Kim on 2016-06-30.
+//  Copyright © 2016 Yevhen Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GrreterDelegator : NSObject
+
+@end
---
+++ FoodTruckP1/FoodTruckP1/GrreterDelegator.m
@@ -0,0 +1,13 @@
+//
+//  GrreterDelegator.m
+//  FoodTruckP1
+//
+//  Created by Yevhen Kim on 2016-06-30.
+//  Copyright © 2016 Yevhen Kim. All rights reserved.
+//
+
+#import "GrreterDelegator.h"
+
+@implementation GrreterDelegator
+
+@end
